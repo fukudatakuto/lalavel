a0366aff3768d0f154534922d2ec204e07c48a71
fukudatakuto <takuto12022002@gmail.com>
2022-03-25 09:54:52 +0900

    3/24

---
+++ laravelapp/tests/Feature/HelloTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\User;
+
+class HelloTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testHello()
+    {
+        $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route'); 
+        $response->assertStatus(404);
+    }
+}
